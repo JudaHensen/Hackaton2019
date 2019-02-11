02e5f477a6e3dba83482e67050d54f09ddf1f87c
JudaHensen <24311@ma-web.nl>
2019-02-11 14:18:06 +0100

    Added template json

--- js/button.js
+++ js/button.js
@@ -6,14 +6,23 @@ class button {
 
     let text = document.createElement('p');
     text.innerHTML = message;
+    text.html = 'button_text';
 
     let img = new Image();
     img.src = image;
+    image.className = 'button_image';
 
     button.appendChild(text);
     button.appendChild(img);
 
     element.appendChild(button);
+
+    let str = '';
+    for(let i = 0; i < element.children.length; i++) {
+      str += '1fr ';
+    }
+    element.style.gridTemplateColumns = str;
+
   }
 
 
--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@ const buttonHolder = document.getElementById('button_holder');
 
 var year = 1980;
 
+function start() {
 
+}
 
+function restart() {
+  year = 1980;
+  
+  removeButtons();
+  start();
+}
 
+function removeButtons() {
+  while (buttonHolder.firstChild) {
+    buttonHolder.removeChild(buttonHolder.firstChild);
+  }
+}
 
 
 
